dfa3c99cd7601115b0404e1d59e9f3ab00df551d
Lolocatcraft <lolodigital@outlook.com>
2026-06-22 15:25:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Like() {
+    const LikeImg = document.querySelector("Like");
+    localStorage.setItem("Gostou", "DinheiroInfinito");
+}
+
+function LikeCars() {
+    const LikeImg = document.querySelector("LikeCars");
+    localStorage.setItem("Gostou", "Cars");
+}
