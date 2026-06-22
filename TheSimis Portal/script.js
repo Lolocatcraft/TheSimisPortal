cb3ab3e895f5a0b873f6a8cddda0d89e480a0107
Lolocatcraft <lolodigital@outlook.com>
2026-06-22 15:24:41 -0300

    Delete TheSimis Portal directory

--- TheSimis Portal/script.js
+++
@@ -1,9 +0,0 @@
-function Like() {
-    const LikeImg = document.querySelector("Like");
-    localStorage.setItem("Gostou", "DinheiroInfinito");
-}
-
-function LikeCars() {
-    const LikeImg = document.querySelector("LikeCars");
-    localStorage.setItem("Gostou", "Cars");
-}
